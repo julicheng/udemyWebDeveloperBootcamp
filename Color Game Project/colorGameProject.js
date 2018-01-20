5d518b30dd41e84e8f19f728a5643772371d6ffa
Juli Cheng <teachjuli@gmail.com>
2018-01-20 15:02:45 +0000

    add colorgameproject files and complete part 1

---
+++ Color Game Project/colorGameProject.js
@@ -0,0 +1,29 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll('.square');
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById('colorDisplay');
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+	//add event listener when clicked
+	squares[i].addEventListener('click', function() {
+		//grab color of clicked square and compare to pickedColor
+		var clickedColor = this.style.backgroundColor;
+		if (clickedColor === pickedColor) {
+			alert('correct');
+		} else {
+			alert('wrong sqaure');
+		}
+	});
+}
