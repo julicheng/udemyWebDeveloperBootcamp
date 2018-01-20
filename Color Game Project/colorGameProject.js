ad34735ac3f8f6e7d64fa5573f5f3f20351f0ec8
Juli Cheng <teachjuli@gmail.com>
2018-01-20 15:42:31 +0000

    add generate random colors function and change h1 background when correct

--- Color Game Project/colorGameProject.js
+++ Color Game Project/colorGameProject.js
@@ -1,16 +1,20 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+//for colors that are not random
+// var colors = [
+// 	"rgb(255, 0, 0)",
+// 	"rgb(255, 255, 0)",
+// 	"rgb(0, 255, 0)",
+// 	"rgb(0, 255, 255)",
+// 	"rgb(0, 0, 255)",
+// 	"rgb(255, 0, 255)"
+// ];
+
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickedColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.getElementById('message');
+var h1 = document.querySelector('h1');
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +28,7 @@ for (var i = 0; i < squares.length; i++) {
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(pickedColor);
+			h1.style.backgroundColor = pickedColor;
 		} else {
 			//fade out if incorrect
 			this.style.backgroundColor = "#232323";
@@ -45,3 +50,24 @@ function pickedColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = []
+	//add random colors to array
+	for (var i = 0; i < num; i++) {
+		arr.push(randomColor());
+	}
+	//return the array
+	return arr;
+}
+
+function randomColor() {
+	//pick a red from 0-255
+	var r = Math.floor(Math.random() * 256);
+	//pick a green from 0-255
+	var g = Math.floor(Math.random() * 256);
+	//pick a blue from 0-255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b +  ")";
+};
