6d0f033425a1e1a43d1b99fdbb9a41559f9b3713
Juli Cheng <teachjuli@gmail.com>
2018-01-21 12:57:21 +0000

    change first to first-of-type

--- jQuery/selectorExercise/selectorExercise.js
+++ jQuery/selectorExercise/selectorExercise.js
@@ -1,4 +1,5 @@
 $('div').css('background', 'purple');
 $('.highlight').css('width', '200px');
 $('#third').css('border', 'orange dashed 2px');
-$('div:first').css('color', 'pink');
+$('div:first-of-type').css('color', 'pink');
+//first-of-type faster than first since first is built in jquery and first-of-type is css
