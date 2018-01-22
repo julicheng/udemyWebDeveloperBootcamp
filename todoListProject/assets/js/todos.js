be4141a4bd778e55c155e8e142ad86b5ddf9c10e
Juli Cheng <teachjuli@gmail.com>
2018-01-22 13:56:51 +0000

    add remove li function

--- todoListProject/assets/js/todos.js
+++ todoListProject/assets/js/todos.js
@@ -3,3 +3,11 @@
 $('li').click(function() {
 	$(this).toggleClass('completed');
 })
+
+//click on x to delete todo
+$('span').click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	}); //remove parent which is li
+	event.stopPropagation(); //to prevent bubbling
+})
