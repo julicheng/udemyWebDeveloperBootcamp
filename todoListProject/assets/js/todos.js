4486228c9edbaaa540cd2dd60d2cece8879907d3
Juli Cheng <teachjuli@gmail.com>
2018-01-22 14:09:03 +0000

    add feature to add new todos

--- todoListProject/assets/js/todos.js
+++ todoListProject/assets/js/todos.js
@@ -1,13 +1,27 @@
 //check off specfic todos by clicking
-
-$('li').click(function() {
+//add on click to parent (ul) rather than lis because lis will 
+//change in future
+//when li is clicked inside of ul
+$('ul').on('click', 'li', function() {
 	$(this).toggleClass('completed');
 })
 
 //click on x to delete todo
-$('span').click(function(event) {
+//when span is clicked inside of a ul
+$('ul').on('click', 'span', function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	}); //remove parent which is li
 	event.stopPropagation(); //to prevent bubbling
+})
+
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		//grab new todo text from input
+		var todoText = $(this).val() 
+		//create a new li and add to ul
+		$('ul').append("<li><span>X</span> " + todoText + "</li>");
+		//clear input
+		$(this).val("");
+	}
 })
